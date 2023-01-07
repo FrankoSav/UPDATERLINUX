af45d8dd72eaf220a3319d3ea31239b49d14e1c4
Franko$avage <117610367+FrankoSav@users.noreply.github.com>
2023-01-07 03:09:04 -0300

    Create Updater.sh

---
+++ Updater.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Menú
+while :
+do
+    clear
+    toilet -f small "FrankoSav"
+    
+    echo "1. Actualizar sistema"
+    echo "2. Actualizar aplicaciones"
+    echo "3. Salir"
+    echo
+    read -p "Selecciona una opción: " opcion
+    case $opcion in
+        1)
+            # Actualizar sistema
+            sudo apt-get update && sudo apt-get upgrade -y
+            toilet -f small "Actualizado Rey"
+            read -p "Presiona Enter para continuar"
+        ;;
+        2)
+            # Actualizar aplicaciones
+            sudo parrot-upgrade -y
+            toilet -f small "Actualizado Rey"
+            read -p "Presiona Enter para continuar"
+        ;;
+        3)
+            # Salir
+            break
+        ;;
+        *)
+            # Opción inválida
+            echo "Opción inválida"
+            read -p "Presiona Enter para continuar"
+        ;;
+    esac
+done
