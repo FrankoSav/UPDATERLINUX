955bd66631acc9d4e764c8e883ab39326053eaf1
FrankoSav <117610367+FrankoSav@users.noreply.github.com>
2023-02-06 16:23:13 +0100

    Update and rename Updater.sh to main.sh

---
+++ main.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Menu
+while :
+do
+    clear
+    figlet "UPDATERLINUX"
+    echo "By:FrankoSav"
+    
+    echo "1- Update Kali Linux"
+    echo "2- Update Parrot Os"
+    echo "3- Exit"
+    echo
+    read -p "Select A Option: " opcion
+    case $opcion in
+        1)
+            # Update Linux 
+            sudo apt-get update && sudo apt-get upgrade -y
+            sudo apt dist-upgrade
+            toilet -f small "Done King"
+            read -p "Press Enter to continue"
+        ;;
+        2)
+            # Update Parrot
+            sudo parrot-upgrade -y
+            git fetch --all
+            git pull --all
+            toilet -f small "Done King"
+            read -p "Press Enter to continue"
+        ;;
+        3)
+            # Exit
+            break
+        ;;
+        *)
+            # Invalid Option
+            echo "Invalid Option"
+            read -p "Press Enter to continue"
+        ;;
+    esac
+done
